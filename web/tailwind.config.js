e0559ab17b61657671c1f7f371ce94c1a0e1dba6
Luan <barkon32@tuta.io>
2023-04-29 14:18:05 -0300

    definida fonte Inter global

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,24 +1,29 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'background': '#1A1D23',
-        'card-background': '#202129',
-        'text': '#F5F5F5',
-        'button': '#71717A',
-      }
+        background: "#1A1D23",
+        "card-background": "#202129",
+        text: "#F5F5F5",
+        button: "#71717A",
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
     },
   },
   plugins: [],
-}
+};
